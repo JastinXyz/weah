33735276b7d67fcb7c70aeaa010985f796cb9bc5
JastinXyz <jastinlinggar@gmail.com>
2022-10-24 12:20:57 +0700

    config

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
 } = require("@adiwajshing/baileys");
 const { Boom } = require("@hapi/boom");
 const P = require("pino");
+let { prefix, autoRead } = require('./config.json');
 
 async function connectToWhatsApp() {
     const { state, loadState, saveState } = useSingleFileAuthState('./session.json');
@@ -34,7 +35,42 @@ async function connectToWhatsApp() {
     })
 
     sock.ev.on('messages.upsert', async m => {
-        console.log(JSON.stringify(m, undefined, 2))
+        var msg = m.messages[0];
+        if (!m || !msg.message) return;
+        if (msg.key && msg.key.remoteJid === "status@broadcast") return;
+        const type = getContentType(msg.message);
+        var text =
+            type === "conversation" && msg.message.conversation
+              ? msg.message.conversation
+              : type == "imageMessage" && msg.message.imageMessage.caption
+              ? msg.message.imageMessage.caption
+              : type == "documentMessage" && msg.message.documentMessage.caption
+              ? msg.message.documentMessage.caption
+              : type == "videoMessage" && msg.message.videoMessage.caption
+              ? msg.message.videoMessage.caption
+              : type == "extendedTextMessage" && msg.message.extendedTextMessage.text
+              ? msg.message.extendedTextMessage.text
+              : type == "listResponseMessage"
+              ? msg.message.listResponseMessage.singleSelectReply.selectedRowId
+              : type == "buttonsResponseMessage" &&
+                msg.message.buttonsResponseMessage.selectedButtonId
+              ? msg.message.buttonsResponseMessage.selectedButtonId
+              : type == "templateButtonReplyMessage" &&
+                msg.message.templateButtonReplyMessage.selectedId
+              ? msg.message.templateButtonReplyMessage.selectedId
+              : "";
+
+        if(autoRead) {
+            await sock.readMessages([{
+                remoteJid: msg.key.remoteJid,
+                id: msg.key.id,
+                participant: msg.key.participant
+            }])
+        }
+
+        if(text === `${prefix}ping`) {
+            await sock.sendMessage(msg.key.remoteJid, { text: 'pong!' })
+        }
     })
 }
 
